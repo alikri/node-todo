ddf8d50cb9c2958059530d40e4d30266702663ec
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-19 23:08:55 +0700

    build: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,21 +4,19 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  overrides: [
-    {
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': ['error', { singleQuote: true }],
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
   },
 };

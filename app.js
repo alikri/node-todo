556730613bde81131f27ca86d016d32b97b8002e
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-19 18:42:05 +0700

    feat: add express set up

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import express from 'express';
-const alacoda = 'mari';
+const app = express();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const port = process.envPORT || 3000;
+
+app.use('/assets', express.static(path.join(__dirname, '/public')));
+
+app.set('view engine', 'ejs');
+app.listen(port);
